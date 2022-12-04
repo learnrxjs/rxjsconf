6a9b1f7aee8727bc90c6a12e564f92b9e36f9ce0
Sam Bulatov <mephistorine@gmail.com>
2022-12-04 21:54:17 +0300

    fix: SSR отключен

--- astro.config.js
+++ astro.config.js
@@ -1,17 +1,15 @@
 import { defineConfig } from "astro/config";
 import solid from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  integrations: [solid(), tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  })],
-  adapter: node({
-    mode: "standalone"
-  })
+  integrations: [
+    solid(),
+    tailwind({
+      config: {
+        applyBaseStyles: false
+      }
+    })
+  ]
 });
